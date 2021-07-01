7bc6840ea0d334c58455bb7fca0cca32d06ea273
Shakila Marando <marandoshakila1@gmail.com>
2021-07-01 12:42:10 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const myButton = document.querySelector();
+
+myButton.style.color = "red";
+
+myButton.innerText = "Chenged";
+
+myButton.addEventListener("mouseout", function () {
+  alert("hello");
+});
